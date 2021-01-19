81c8f231113ee8253837cc2325450b67e64de226
Mike Eling <mike.eling97@gmail.com>
2021-01-19 01:21:51 +0100

    :pencil: Update project url

--- src/components/common/Projects/list.ts
+++ src/components/common/Projects/list.ts
@@ -4,7 +4,7 @@ const projects = [
         logo: "https://i.imgur.com/VzOVjUy.png",
         description: "Circles is a messaging app designed with family ties in mind. It makes sure that you not forget to catch up with a family member and never forget about their birthday ever again. Thanks to this app, your family relationships will be better than ever!",
         url: "https://circles.assembless.space/",
-        repo_url: "https://github.com/DRFR0ST/circles-web"
+        repo_url: "https://github.com/Assembless/circles-web"
     },
     {
         name: "react-littera",
